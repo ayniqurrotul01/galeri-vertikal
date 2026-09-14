ee055d845ff56016d32e566efe74df0fd00d92dd
ayniqurrotul01 <ayniqurrotul@gmail.com>
2026-09-15 01:27:57 +0700

    Tambahkan app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "id.ayni.galerivertikal"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "id.ayni.galerivertikal"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+}
