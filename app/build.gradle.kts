96639e102360980c9e71b3a9a65e5eb8873d6f50
ayniqurrotul01 <ayniqurrotul@gmail.com>
2026-09-15 09:50:56 +0700

    Naikkan versi ke 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "id.ayni.galerivertikal"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     buildTypes {
